c90ac06a1ee47b75960f54919618845ac59a7792
Michael <hovgaard1990@gmail.com>
2022-11-09 13:26:40 +0100

    Wuuuat

--- VisitAarhus/src/App.js
+++ VisitAarhus/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*App Component
  *
  *Main App fectching a list of places in json and rendering them
@@ -15,6 +16,33 @@ class App extends Component {
         <h1>Where should we eat? test test test</h1>
         <div className="container">
           <Places />
+=======
+import { Route, Routes } from 'react-router-dom';
+import React, { Component } from 'react'
+
+import Home from './Pages/Home.js'
+import Discover from './Pages/discover.jsx'
+import Personalpage from './Pages/Gunvarsplan.jsx'
+import Fejl from './Pages/fejl.js';
+
+
+class App extends Component {
+
+    render()
+    {
+
+      return(
+        <div className="App">
+          <Routes> 
+            <Route path='/*' element={<Home/>}/>
+            <Route path='/discover' element={<Discover/>}/>
+            <Route path='/Personalspace' element={<Personalpage/>}/>
+            <Route path='/*' element={<Fejl/>}/>
+
+          
+          
+          </Routes>
+>>>>>>> Stashed changes
         </div>
       </div>
     );
