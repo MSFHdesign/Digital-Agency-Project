d6c163f62b1336298499a7d0c2735749ea7977f0
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-11-09 13:24:59 +0100

    Update App.js

--- VisitAarhus/src/App.js
+++ VisitAarhus/src/App.js
@@ -1,27 +1,24 @@
 /*App Component
-*
-*Main App fectching a list of places in json and rendering them
-*
-*
-*Author Jeffrey Serio
-*/
-import React, { Component } from 'react'
-import Places from './components/place/Places'
+ *
+ *Main App fectching a list of places in json and rendering them
+ *
+ *
+ *Author Jeffrey Serio
+ */
+import React, { Component } from "react";
+import Places from "./components/place/Places";
 
 class App extends Component {
-
-    render()
-    {
-
-      return(
-        <div className="App">
-          <h1>Where should we eat?</h1>
-          <div className="container">
-            <Places/>
-          </div>
+  render() {
+    return (
+      <div className="App">
+        <h1>Where should we eat? test test test</h1>
+        <div className="container">
+          <Places />
         </div>
-      )
-    }
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
