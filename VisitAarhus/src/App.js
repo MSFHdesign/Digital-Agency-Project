3dc2f34aa5dee312e1969177c0dbcc162aee2176
Michael <hovgaard1990@gmail.com>
2022-11-10 10:57:20 +0100

    .

--- VisitAarhus/src/App.js
+++ VisitAarhus/src/App.js
@@ -6,14 +6,21 @@ import Discover from './Pages/discover.js'
 import Personalpage from './Pages/Gunvarsplan.js'
 import Fejl from './Pages/fejl.js';
 
-import Nav from './components/NavTop/Nav.jsx';
+import LogoTop from './components/LogoTop/LogoTop' 
+import Nav from './components/NavTop/Nav'
+
 import Searchbar from './components/NavTop/Searchbar.jsx'
+
+
 function App() {
   return (
         <div className="App">
-          <Nav />
-          <Searchbar />
-          <Routes> 
+          <div className='HeaderTop Flexbox dirRow'>
+        
+            <LogoTop/> <Nav/> 
+          </div>
+            <Searchbar />
+            <Routes> 
 
               <Route path='/' element={<Home/>}/>
               <Route path='/discover' element={<Discover/>}/>
--- VisitAarhus/src/components/Header/header.jsx
+++ VisitAarhus/src/components/Header/header.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
-import { Box } from '@mui/system'
-import LogoTop from '../LogoTop/LogoTop' 
-import Nav from '../NavTop/Nav'
-
-import './headerstyle.css'; 
 
 export default function header() {
   return (
-    <Box ClassName='HeaderTop Flexbox dirColumn'>
-        
-         <LogoTop/> 
-         
-         
-
-        <Nav />
-
-
-    </Box>
+    <div>header</div>
   )
 }
