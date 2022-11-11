a007d5c3ab8ed9079e0ce4bfbdfb04c6b3c36a7d
Skydstofte <rikke-sc@hotmail.com>
2022-11-11 11:20:46 +0100

    i

--- VisitAarhus/src/Pages/Home.js
+++ VisitAarhus/src/Pages/Home.js
@@ -7,13 +7,15 @@ export default function Home() {
   return (
   
 
-    <div className='BoxContainer' ><h2>Home</h2>
-    <div className='frameContainer'>
-      <img className='headerImg' src={aarhusNightLife} alt = 'Aarhus dining'/>
-      <div className='insideFrame'></div>
-      <h2 className='exsploreTitle'>Restaurants</h2>
+    <div className='BoxContainer'>
+      {/* <h2>Home</h2> */}
+      <div className='frameContainer'>
+        <img className='headerImg' src={aarhusNightLife} alt = 'Aarhus dining'/>
+        <div className='insideFrame'></div>
+        <h2 className='exsploreTitle'>Restaurants</h2>
+      </div>
+      <Basket/>
     </div>
-    <Basket/></div>
  
   
   
