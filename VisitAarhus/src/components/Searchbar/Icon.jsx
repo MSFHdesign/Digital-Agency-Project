e334954c05eeb82d30b34004a26f7b7deef163ce
MultimedieGunvor <eaagupa@students.eaaa.dk>
2022-11-11 10:29:07 +0100

    searchbar

--- VisitAarhus/src/components/Searchbar/Icon.jsx
+++ VisitAarhus/src/components/Searchbar/Icon.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import searchIcon from '../../Picture/icons/search.png';
 
 export default function Icon() {
   return (
-    <div> Icon </div>
+    <img src={searchIcon} alt="Search icon" style={{height: "30px"}}/>
   )
 }
