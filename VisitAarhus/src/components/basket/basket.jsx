e3f6d3c9eb11feb5189064d261a70ab66d719a3a
Michael <hovgaard1990@gmail.com>
2022-11-14 12:31:52 +0100

    Update basket.jsx

--- VisitAarhus/src/components/basket/basket.jsx
+++ VisitAarhus/src/components/basket/basket.jsx
@@ -2,21 +2,30 @@
 import Draggable from 'react-draggable';
 import './basketStyle.css'
 import calendar from '../../Picture/icons/calendar.png';
-import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
+
+
+
 
 
 export default function basket() {
 // const [tripN, setTripN] = useState([]);
   
+  
+ const [trip, setTrips] = useState(0);
+  
   return (
+
     <Draggable bounds="parent">
       
-      <Link to='/seetrip'>
+     
         <div className='BasketDragable'>
-          
-        <img className='menuItemImg' src={calendar} alt = 'MenuIcon'/>
+          <div> <p>1</p> </div>
+        <img className='menuItemImg' src={calendar} alt='MenuIcon' />
+     
         </div>
-      </Link>
+     
     </Draggable>
   )
 }
