63832e3ea9e2ef67fec1f1d5a761d06438020eca
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-11-14 12:10:49 +0100

    Update planTrip.js

--- VisitAarhus/src/Pages/planTrip.js
+++ VisitAarhus/src/Pages/planTrip.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AddArticle from "../components/firebase tilfoej copy";
+import aarhusNightLife from "../Picture/img/aarhus-nightlife.jpg";
+
 export default function planTrip() {
   return (
     <div className="plantrip">
+      <div className="frameContainer">
+        <img className="headerImg" src={aarhusNightLife} alt="Aarhus dining" />
+        <div className="insideFrame"></div>
+        <h2 className="exsploreTitle">Home</h2>
+      </div>
+
       <h1>Create new calender</h1>
       <p>
         Here you can create a new calender for your trip! Just give it a name
