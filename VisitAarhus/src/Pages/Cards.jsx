95eedea07a03213c6afa3f746af01398813634fb
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-11-15 11:01:05 +0100

    sådan michael

--- VisitAarhus/src/Pages/Cards.jsx
+++ VisitAarhus/src/Pages/Cards.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Accordion = ({ title, adr, type, zip, city, phone }) => {
+const Accordion = ({ title, adr, type, zip, city, phone, type2 }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className={type} >
-      <div className="accordion-show" onClick={() => setIsActive(!isActive)}>
-        <div className='a'>{title}</div>
-        <div>{isActive ? '-' : '+'}</div>
+    <div className={type}>
+      <div
+        className={isActive ? type2 : "accordion-show"}
+        onClick={() => setIsActive(!isActive)}
+      >
+        <div className="a">{title}</div>
+        <div className={isActive ? "ned" : "henad"}>{isActive ? ">" : ">"}</div>
       </div>
-          {isActive && <div classname='inside'>{adr} {zip} {city} {phone} </div>}
+      {isActive && (
+        <div className="inside">
+          <p>
+            {adr} {zip} {city}
+          </p>
+          <p>{phone}</p>
+        </div>
+      )}
     </div>
   );
 };
